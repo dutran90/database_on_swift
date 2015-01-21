bd3e9bbb2a3a411b751ae8a778a4d0ad0247ecee
dutran90 <ngocdu12@gmail.com>
2015-01-21 11:19:43 +0700

    table view Main to choose type of database

--- DatabaseTypeOnSwift/ViewController.swift
+++ DatabaseTypeOnSwift/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tvMain: UITableView!
+    
+    var items = ["NSUserdefault":"Use NSUserdefault to save data","Plist file":"Import plist file to manage data","CoreData":"Choose check box CoreData when new project", "SQLlite":"Using SQLlite packed", "Text file":"Import file text to manage data", "XML file":"Using XML file to manage data"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tvMain.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tvMain: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(tvMain: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        var arrayKeys = [String](items.keys)
+        
+        var arrayValues = [String](items.values)
+        
+        cell.textLabel?.text = arrayKeys[indexPath.row]
+        
+        cell.textLabel?.textColor = UIColor.blueColor()
+        
+        cell.detailTextLabel?.text = arrayValues[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tvMain: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
+        
+    }
+    
 }
 
